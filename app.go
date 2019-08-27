ad51461356c5edbd67406ad985dc7d2008c1476d
Just1689 <Just1689@gmail.com>
2019-08-28 00:20:48 +0200

    fetch data for the user

--- app.go
+++ app.go
@@ -5,11 +5,15 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/just1689/entity-sync/es"
 	"github.com/just1689/entity-sync/es/shared"
+	"github.com/just1689/pg-gateway/client"
+	"github.com/just1689/pg-gateway/query"
 	"github.com/sirupsen/logrus"
 	"net"
 	"net/http"
 	"os"
 )
+
+var entitySync es.EntitySync
 
 func main() {
 
@@ -20,7 +24,7 @@ func main() {
 		WSPassThrough: passThrough,
 	}
 	//Setup entitySync with that configuration
-	entitySync := es.Setup(config)
+	entitySync = es.Setup(config)
 
 	//Register an entity and tell the library how to fetch and what to write to the client
 	entitySync.RegisterEntityAndDBHandler("sigui", func(entityKey shared.EntityKey, secret string, handler shared.ByteHandler) {
@@ -35,5 +39,45 @@ func main() {
 }
 
 func passThrough(secret string, b []byte) {
-	logrus.Println(string(b))
+	p := PassThroughMsg{}
+	err := json.Unmarshal(b, &p)
+	if err != nil {
+		logrus.Errorln("error while unmarshaling passThroughMsg from client ws")
+		logrus.Errorln(err)
+		return
+	}
+
+	//The client is requesting some data
+	go sendTableToClient(p.Topic, secret)
+}
+
+func sendTableToClient(table string, secret string) {
+	pgg := os.Getenv("pgGWAddr")
+	c, err := client.GetEntityManyAsync(pgg, query.Query{
+		Entity: "items",
+		Comparisons: []query.Comparison{
+			query.Comparison{
+				Field:      "title",
+				Comparator: "eq",
+				Value:      table,
+			},
+		},
+	})
+	if err != nil {
+		logrus.Errorln(err)
+		return
+	}
+
+	for row := range c {
+		entitySync.Bridge.NotifyAllOfChange(
+			shared.EntityKey{
+				Entity: shared.EntityType(secret),
+				ID:     string(row),
+			})
+	}
+
+}
+
+type PassThroughMsg struct {
+	Topic string `json:"topic"`
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.12
 require (
 	github.com/gorilla/mux v1.7.3
 	github.com/just1689/entity-sync v0.0.0-20190613100129-755dc07e8656
+	github.com/just1689/pg-gateway v0.0.0-20190827185244-576d5359707e
+	github.com/sirupsen/logrus v1.4.2
 )
